098cbe07ae3d8aba4e4aa084a3d734414faf81d7
jokerro <d.jokerro@gmail.com>
2014-11-06 12:59:23 +0200

    Дополнен алгоритм сохранения списка смежности.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
    QCoreApplication a(argc, argv);
    Graph *temp=new Graph();
-   temp->ReadMatrix("E:\\ProgrammFiles\\Graph-Framework\\mat.txt");
-   saveMatrix::save("E:\\ProgrammFiles\\Graph-Framework\\saved.txt", temp);
+   temp->ReadMatrix("mat.txt");
+   saveMatrix::save("saved.txt", temp);
    delete temp;
    return a.exec();
 }
