f5ac55046d0f37206ce0ba8bedf2764b739f94f2
mrev1l1 <ds.noob11@gmail.com>
2014-11-06 23:25:33 +0200

    DEMO

--- main.cpp
+++ main.cpp
@@ -6,18 +6,11 @@
 int main(int argc, char *argv[])
 {
    QCoreApplication a(argc, argv);
-   Graph *temp=new Graph();
-   temp->ReadMatrix("mat.txt");
-   saveMatrix::save("saved.txt", temp);
 
-   Graph* t1 = new Graph();
-   t1->ReadMatrix("D:\\Qt\\Tools\\QtCreator\\path2.txt");
-   saveMatrix::save("D:\\Qt\\Tools\\QtCreator\\path2_saved.txt",t1);
-   /*Graph t2;
-   t2.ReadMatrix("D:\\Qt\\Tools\\QtCreator\\path2.txt");
-   bool tr;
-   //tr=(t1==t2);*/
-   delete t1;
-   delete temp;
+   Graph *temp=new Graph();
+     temp->ReadMatrix("D:\\Qt\\Tools\\QtCreator\\bin\\Graph_framework\\mat.txt");
+     saveMatrix::save("D:\\Qt\\Tools\\QtCreator\\bin\\Graph_framework\\saved.txt", temp);
+     delete temp;
+
    return a.exec();
 }
